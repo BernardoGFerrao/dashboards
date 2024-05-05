d89f41697022361a9754e6d45148521b35302391
Bernardo Ferrão <bernardo.f@inf.ufpel.edu.br>
2024-05-05 17:27:35 -0300

    Dashboards

---
+++ introducao.py
@@ -0,0 +1,3 @@
+# 2 Formas de Fazer:
+# Usar o Python para fazer os tratamentos de bases de dados, criar colunas, etc. para importar para o Power BI depois -> biblioteca pandas -> exatamente como aprendemos no módulo de pandas
+# (O que vamos focar aqui). Usar scripts em Python diretamente dentro do Power BI
